a1d40df42b9373248dbb38f3c4701064d08cd29a
quangminh4102008 <144684151+quangminh4102008@users.noreply.github.com>
2023-09-26 22:32:29 +0700

    Bai 10

---
+++ class folder/Bai 10/vdu 4.c
@@ -0,0 +1,16 @@
+//
+// Created by haizz on 9/26/2023.
+//
+#include <stdio.h>
+#include <conio.h>
+//void main()
+{
+    int cnt;
+
+    for (cnt = 1; cnt <= 10; cnt ++)
+    {
+        if (cnt == 5)
+            break;
+        printf("%d\t", cnt);
+    }
+}
