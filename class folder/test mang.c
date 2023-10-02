bf0e484640c4edc4e6d0f723644673316e567efc
quangminh4102008 <144684151+quangminh4102008@users.noreply.github.com>
2023-10-02 18:26:44 +0700

    test mang

---
+++ class folder/test mang.c
@@ -0,0 +1,23 @@
+//
+// Created by haizz on 10/2/2023.
+//
+#include <stdio.h>
+//int test(){
+    int main()
+    {
+        int arr [10];
+        int i;
+        arr[0] = 1;
+        arr[1] = 10;
+                arr [2]= 20;
+                arr[3]= 30;
+                arr[4] = 40;
+                arr [5] = 50;
+                arr [6] = 60;
+                arr [7] = 70;
+                arr[8] = 80;
+                arr [9] = 90;
+                for (i=0; i<10;i++)
+                    printf("%d\n",arr[i]);
+                return 0;
+    }
